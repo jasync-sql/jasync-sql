be3d489177703cf9c05dd6425ba4a3cbe744e113
Mauricio Linhares <mauricio.linhares@gmail.com>
2013-04-20 11:04:53 -0300

    Adding licence to new files that did not have it

--- src/main/scala/com/github/mauricio/async/db/util/ArrayStreamingParser.scala
+++ src/main/scala/com/github/mauricio/async/db/util/ArrayStreamingParser.scala
@@ -1,14 +1,24 @@
+/*
+ * Copyright 2013 Maurício Linhares
+ *
+ * Maurício Linhares licenses this file to you under the Apache License,
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
 package com.github.mauricio.async.db.util
 
 import scala.collection.mutable.StringBuilder
 import scala.collection.mutable
 import com.github.mauricio.async.db.postgresql.exceptions.InvalidArrayException
 
-/**
- * User: mauricio
- * Date: 4/19/13
- * Time: 1:31 PM
- */
 object ArrayStreamingParser {
 
   def parse(content: String, delegate: ArrayStreamingParserDelegate) {
