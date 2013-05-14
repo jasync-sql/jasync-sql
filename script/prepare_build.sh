fe1737e5a560e8edc654f3c842645f51d5417e67
Mauricio Linhares <mauricio.linhares@gmail.com>
2013-05-14 09:56:29 -0300

    Fixing MySQL command

--- script/prepare_build.sh
+++ script/prepare_build.sh
@@ -2,7 +2,7 @@
 
 echo "Preparing MySQL configs"
 mysql -u root -e 'create database mysql_async_tests;'
-mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO ‘mysql_async’@'localhost’ IDENTIFIED BY ‘root’ WITH GRANT OPTION";
+mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'mysql_async'@'localhost' IDENTIFIED BY 'root' WITH GRANT OPTION";
 
 echo "preparing postgresql configs"
 
@@ -12,6 +12,10 @@ psql -c "CREATE USER postgres_cleartext WITH PASSWORD 'postgres_cleartext'; GRAN
 psql -c "CREATE USER postgres_kerberos WITH PASSWORD 'postgres_kerberos'; GRANT ALL PRIVILEGES ON DATABASE netty_driver_test to postgres_kerberos;" -U postgres
 
 sudo chmod 777 /etc/postgresql/9.1/main/pg_hba.conf
+
+echo "pg_hba.conf goes as follows"
+cat "/etc/postgresql/9.1/main/pg_hba.conf"
+
 sudo echo "host     all             postgres_md5    127.0.0.1/32            md5" > /etc/postgresql/9.1/main/pg_hba.conf
 sudo echo "host     all             postgres_cleartext 127.0.0.1/32         password" > /etc/postgresql/9.1/main/pg_hba.conf
 sudo echo "host     all             postgres_kerberos 127.0.0.1/32         krb5" > /etc/postgresql/9.1/main/pg_hba.conf
