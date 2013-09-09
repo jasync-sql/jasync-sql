eb7032b3c40c592dfa78b3a03184d1dbe64e1457
Mauricio Linhares <mauricio.linhares@gmail.com>
2013-09-08 23:53:29 -0300

    Upgrading netty, fixes #45

--- project/Build.scala
+++ project/Build.scala
@@ -56,7 +56,7 @@ object Configuration {
     "joda-time" % "joda-time" % "2.2",
     "org.joda" % "joda-convert" % "1.3.1",
     "org.scala-lang" % "scala-library" % "2.10.2",
-    "io.netty" % "netty-all" % "4.0.7.Final",
+    "io.netty" % "netty-all" % "4.0.9.Final",
     specs2Dependency,
     logbackDependency
   )
