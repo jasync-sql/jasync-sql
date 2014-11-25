993f8f1fda8b26df2b1db147fb1203df3576bd98
Dylan Simon <dylan@dylex.net>
2014-11-25 14:54:12 -0500

    Fix encoding of backslashes in arrays

--- postgresql-async/src/test/scala/com/github/mauricio/async/db/postgresql/ArrayTypesSpec.scala
+++ postgresql-async/src/test/scala/com/github/mauricio/async/db/postgresql/ArrayTypesSpec.scala
@@ -34,7 +34,7 @@ class ArrayTypesSpec extends Specification with DatabaseTestHelper {
       (smallint_column, text_column, timestamp_column)
       values (
       '{1,2,3,4}',
-      '{"some,\"comma,separated,text","another line of text",NULL}',
+      '{"some,\"comma,separated,text","another line of text","fake\,backslash","real\\,backslash\\",NULL}',
       '{"2013-04-06 01:15:10.528-03","2013-04-06 01:15:08.528-03"}'
       )"""
 
@@ -52,7 +52,7 @@ class ArrayTypesSpec extends Specification with DatabaseTestHelper {
           executeDdl(handler, insert, 1)
           val result = executeQuery(handler, "select * from type_test_table").rows.get
           result(0)("smallint_column") === List(1,2,3,4)
-          result(0)("text_column") === List("some,\"comma,separated,text", "another line of text", null )
+          result(0)("text_column") === List("some,\"comma,separated,text", "another line of text", "fake,backslash", "real\\,backslash\\", null )
           result(0)("timestamp_column") === List(
             TimestampWithTimezoneEncoderDecoder.decode("2013-04-06 01:15:10.528-03"),
             TimestampWithTimezoneEncoderDecoder.decode("2013-04-06 01:15:08.528-03")
@@ -68,7 +68,7 @@ class ArrayTypesSpec extends Specification with DatabaseTestHelper {
         TimestampWithTimezoneEncoderDecoder.decode("2013-04-06 01:15:08.528-03")
       )
       val numbers = List(1,2,3,4)
-      val texts = List("some,\"comma,separated,text", "another line of text", null )
+      val texts = List("some,\"comma,separated,text", "another line of text", "fake,backslash", "real\\,backslash\\", null )
 
       withHandler {
         handler =>
