6c715874de371c3695e672cf06d5c9a277ac0261
Mikael Ståldal <mst@appearnetworks.com>
2014-12-10 14:56:40 +0100

    Remove unnecessary toString

--- mysql-async/src/main/scala/com/github/mauricio/async/db/mysql/message/client/SendLongDataMessage.scala
+++ mysql-async/src/main/scala/com/github/mauricio/async/db/mysql/message/client/SendLongDataMessage.scala
@@ -3,6 +3,4 @@ package com.github.mauricio.async.db.mysql.message.client
 case class SendLongDataMessage (
                                  statementId : Array[Byte],
                                  value : Any,
-                                 paramId : Int ) {
-  override def toString = "SendLongDataMessage(statementId=" + statementId + ",paramId=" + paramId + ",value.getClass=" + value.getClass.getName +")"
-}
+                                 paramId : Int )
