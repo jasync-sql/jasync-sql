1d343fcef9b7c1fc6f8076724a729fd3cfa7dbe2
Doug Chimento <dchimento@gmail.com>
2018-09-04 20:55:49 +0300

    matching test container with docker container root password

--- mysql-async/src/test/java/com/github/jasync/sql/db/mysql/ContainerHelper.java
+++ mysql-async/src/test/java/com/github/jasync/sql/db/mysql/ContainerHelper.java
@@ -36,23 +36,22 @@ public class ContainerHelper {
   /**
    *   config for container.
    */
-  private static Configuration containerConfiguration = new Configuration(
+  private static Configuration rootConfiguration = new Configuration(
       "root",
       "localhost",
       33306,
-      "root",
+      "test",
       "mysql_async_tests");
 
 
   static {
     try {
-      Connection connection = new MySQLConnection(containerConfiguration).connect().get(1, TimeUnit.SECONDS);
-      connection.sendQuery("GRANT ALL PRIVILEGES ON *.* TO 'mysql_async'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION;").get(1, TimeUnit.SECONDS);
+      new MySQLConnection(rootConfiguration).connect().get(1, TimeUnit.SECONDS);
       log.info("Using local mysql instance {}", defaultConfiguration);
     } catch (Exception e) {
       // If local instance isn't running, start a docker mysql on random port
       if (mysql == null){
-        mysql = new MySQLContainer("mysql:5.7")
+        mysql = new MySQLContainer("mysql:5.6")
                   .withDatabaseName("mysql_async_tests")
                   .withPassword("root")
                   .withUsername("mysql_async");
@@ -61,10 +60,12 @@ public class ContainerHelper {
         mysql.start();
       }
       defaultConfiguration = new Configuration(mysql.getUsername(), "localhost", mysql.getFirstMappedPort(), mysql.getPassword(), mysql.getDatabaseName());
+      rootConfiguration = new Configuration("root", "localhost", mysql.getFirstMappedPort(), "test", "mysql_async_tests");
       log.info("Using test container instance {}", defaultConfiguration);
     } finally {
       try {
-        Connection connection = new MySQLConnection(defaultConfiguration).connect().get(1, TimeUnit.SECONDS);
+        Connection connection = new MySQLConnection(rootConfiguration).connect().get(1, TimeUnit.SECONDS);
+        connection.sendQuery("GRANT ALL PRIVILEGES ON *.* TO 'mysql_async'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION;").get(1, TimeUnit.SECONDS);
         QueryResult r  = connection.sendQuery("select count(*) as cnt  from mysql.user where user = 'mysql_async_nopw';").get(1, TimeUnit.SECONDS);
         if (r.getRows() != null && r.getRows().size() > 0) {
           RowData rd = r.getRows().get(0);
