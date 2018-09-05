cc204c22538fae28df563c59c1497e6fb3158397
oshai <ohadshai@gmail.com>
2018-09-06 00:26:47 +0300

    convert scala to kotlin in postgres

--- postgresql-async/src/main/java/com/github/mauricio/async/db/postgresql/pool/PostgreSQLConnectionFactory.kt
+++ postgresql-async/src/main/java/com/github/mauricio/async/db/postgresql/pool/PostgreSQLConnectionFactory.kt
@@ -1,15 +1,15 @@
 package com.github.mauricio.async.db.postgresql.pool
 
 import com.github.jasync.sql.db.Configuration
+import com.github.jasync.sql.db.exceptions.ConnectionTimeoutedException
 import com.github.jasync.sql.db.pool.ObjectFactory
-import com.github.jasync.sql.db.util.ExecutorServiceUtils
-import com.github.jasync.sql.db.util.NettyUtils
-import com.github.jasync.sql.db.util.Try
+import com.github.jasync.sql.db.util.*
 import com.github.mauricio.async.db.postgresql.PostgreSQLConnection
 import io.netty.channel.EventLoopGroup
 import mu.KotlinLogging
 import java.nio.channels.ClosedChannelException
 import java.util.concurrent.ExecutorService
+import java.util.concurrent.TimeUnit
 
 private val logger = KotlinLogging.logger {}
 
@@ -21,70 +21,70 @@ private val logger = KotlinLogging.logger {}
  */
 
 class PostgreSQLConnectionFactory(val configuration: Configuration,
-                                  group: EventLoopGroup = NettyUtils.DefaultEventLoopGroup,
-                                  executionContext: ExecutorService = ExecutorServiceUtils.CachedThreadPool) : ObjectFactory<PostgreSQLConnection> {
+                                  val group: EventLoopGroup = NettyUtils.DefaultEventLoopGroup,
+                                  val executionContext: ExecutorService = ExecutorServiceUtils.CachedThreadPool) : ObjectFactory<PostgreSQLConnection> {
 
-  fun create: PostgreSQLConnection() {
-    val connection = PostgreSQLConnection(configuration, group = group, executionContext = executionContext)
-    Await.result(connection.connect, configuration.connectTimeout)
+    override fun create(): PostgreSQLConnection {
+        val connection: PostgreSQLConnection = PostgreSQLConnection(configuration, group = group, executionContext = executionContext)
+        connection.connect().get(configuration.connectTimeout.toMillis(), TimeUnit.MILLISECONDS)
 
-    connection
-  }
-
-  fun destroy(item: PostgreSQLConnection) {
-    item.disconnect
-  }
-
-  /**
-   *
-   * Validates by checking if the connection is still connected to the database or not.
-   *
-   * @param item an object produced by this pool
-   * @return
-   */
-
-  fun validate(item: PostgreSQLConnection): Try<PostgreSQLConnection> {
-    Try {
-      if (item.isTimeouted) {
-        throw ConnectionTimeoutedException(item)
-      }
-      if (!item.isConnected || item.hasRecentError) {
-        throw ClosedChannelException()
-      }
-      item.validateIfItIsReadyForQuery("Trying to give back a connection that is not ready for query")
-      item
+        return connection
     }
-  }
 
-  /**
-   *
-   * Tests whether we can still send a **SELECT 0** statement to the database.
-   *
-   * @param item an object produced by this pool
-   * @return
-   */
+    override fun destroy(item: PostgreSQLConnection) {
+        item.disconnect()
+    }
 
-  override fun test(item: PostgreSQLConnection): Try<PostgreSQLConnection> {
-    val result: Try<PostgreSQLConnection> = Try({
-      Await.result(item.sendQuery("SELECT 0"), configuration.testTimeout)
-      item
-    })
+    /**
+     *
+     * Validates by checking if the connection is still connected to the database or not.
+     *
+     * @param item an object produced by this pool
+     * @return
+     */
 
-    result when {
-      Failure(e) -> {
-        try {
-          if (item.isConnected) {
-            item.disconnect
-          }
-        } catch {
-          e : Exception -> log.error("Failed disconnecting object", e)
+    override fun validate(item: PostgreSQLConnection): Try<PostgreSQLConnection> {
+        return Try {
+            if (item.isTimeout()) {
+                throw ConnectionTimeoutedException(item)
+            }
+            if (!item.isConnected() || item.hasRecentError()) {
+                throw ClosedChannelException()
+            }
+            item.validateIfItIsReadyForQuery("Trying to give back a connection that is not ready for query")
+            item
         }
-        result
-      }
-      Success(i) -> {
-        result
-      }
     }
-  }
+
+    /**
+     *
+     * Tests whether we can still send a **SELECT 0** statement to the database.
+     *
+     * @param item an object produced by this pool
+     * @return
+     */
+
+    override fun test(item: PostgreSQLConnection): Try<PostgreSQLConnection> {
+        val result: Try<PostgreSQLConnection> = Try {
+            item.sendQuery("SELECT 0").get(configuration.testTimeout.toMillis(), TimeUnit.MILLISECONDS)
+            item
+        }
+
+        return when (result) {
+            is Failure -> {
+                try {
+                    if (item.isConnected()) {
+                        item.disconnect()
+                    }
+                } catch (e: Exception) {
+                    logger.error("Failed disconnecting object", e)
+                }
+                result
+            }
+            is Success -> {
+                result
+            }
+        }
+    }
 
 }
