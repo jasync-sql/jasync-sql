dba9ec5de3b45035e085dafc3494a9f08e8ca8e0
Doug Chimento <dchimento@gmail.com>
2018-09-05 20:36:57 +0300

    Adding transaction test

--- mysql-async/src/test/java/com/github/jasync/sql/db/mysql/TransactionSpec.kt
+++ mysql-async/src/test/java/com/github/jasync/sql/db/mysql/TransactionSpec.kt
@@ -1,25 +1,49 @@
 package com.github.jasync.sql.db.mysql
 
-//import com.github.jasync.sql.db.util.ExecutorServiceUtils
-//import com.github.jasync.sql.db.util.flatMap
-//import org.junit.Test
-//import kotlin.test.assertEquals
-//import kotlin.test.assertNotNull
+import com.github.jasync.sql.db.util.ExecutorServiceUtils
+import com.github.jasync.sql.db.util.flatMap
+import org.junit.Ignore
+import org.junit.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
 
 class TransactionTest : ConnectionHelper() {
 
-//    @Test
-//    fun transaction_should_complete() {
-//        withConnection { conn ->
-//            executeQuery(conn, this.createTable)
-//            awaitFuture(conn.inTransaction(ExecutorServiceUtils.CachedThreadPool) {
-//                conn.sendPreparedStatement(this.insert).flatMap(ExecutorServiceUtils.CachedThreadPool) { _ -> conn.sendPreparedStatement(this.insert) }
-//            })
-//            val result = assertNotNull(executeQuery(conn, this.select))
-//            assertNotNull(result)
-//            assertEquals(2, result.rowsAffected)
-//            assertEquals("Boogie Man", result.rows?.get(0)?.get("name"))
-//            assertEquals("Boogie Man", result.rows?.get(1)?.get("name"))
-//        }
-//    }
+    @Test
+    fun transaction_should_complete() {
+        withConnection { conn ->
+            executeQuery(conn, this.createTable)
+            val future = conn.inTransaction(ExecutorServiceUtils.CachedThreadPool) {
+                c -> c.sendQuery(this.insert).flatMap(ExecutorServiceUtils.CachedThreadPool) { _ -> conn.sendQuery(this.insert) }
+            }
+            val r = awaitFuture(future)
+            assertEquals(1, r.rowsAffected)
+            val result = assertNotNull(executePreparedStatement(conn, this.select))
+            assertNotNull(result)
+            assertEquals(2, result.rowsAffected)
+            assertEquals("Boogie Man", result.rows?.get(0)?.get("name"))
+            assertEquals("Boogie Man", result.rows?.get(1)?.get("name"))
+        }
+
+    }
+
+    @Test
+    @Ignore
+    fun transaction_should_complete_prepared() {
+        withConnection { conn ->
+            executeQuery(conn, this.createTable)
+            val future = conn.inTransaction(ExecutorServiceUtils.CachedThreadPool) {
+                c -> c.sendPreparedStatement(this.insert).flatMap(ExecutorServiceUtils.CachedThreadPool) { _ -> conn.sendPreparedStatement(this.insert) }
+            }
+            awaitFuture(future)
+            val result = assertNotNull(executePreparedStatement(conn, this.select))
+            assertNotNull(result)
+            assertEquals(2, result.rowsAffected)
+            assertEquals("Boogie Man", result.rows?.get(0)?.get("name"))
+            assertEquals("Boogie Man", result.rows?.get(1)?.get("name"))
+        }
+
+    }
+
 }
+
