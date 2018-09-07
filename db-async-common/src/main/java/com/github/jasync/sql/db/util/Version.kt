7d4d096240eabd95f343840350c526935d1b109e
oshai <ohadshai@gmail.com>
2018-09-07 21:40:38 +0300

    fix version test and parsing

--- db-async-common/src/main/java/com/github/jasync/sql/db/util/Version.kt
+++ db-async-common/src/main/java/com/github/jasync/sql/db/util/Version.kt
@@ -1,13 +1,13 @@
-
 package com.github.jasync.sql.db.util
 
 //reusing KotlinVersion as it has the same semantics
 typealias Version = KotlinVersion
 
 fun parseVersion(version: String): Version {
-  val splitted = version.split(".")
-  return Version(splitted[0].toIntOrNull() ?: 0,
-      splitted[1].toIntOrNull() ?: 0,
-      splitted[2].toIntOrNull() ?: 0
-  )
+    val splitted = version.split(".")
+    return Version(
+            (if (splitted.isNotEmpty()) splitted[0].toIntOrNull() else null) ?: 0,
+            (if (splitted.size > 1) splitted[1].toIntOrNull() else null) ?: 0,
+            (if (splitted.size > 2) splitted[2].toIntOrNull() else null) ?: 0
+    )
 }
