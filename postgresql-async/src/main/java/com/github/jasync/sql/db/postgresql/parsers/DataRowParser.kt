9ec90f721be554456dadc5ebaf87e1f951a83446
oshai <oshai@outbrain.com>
2018-09-11 00:27:39 +0300

    fix in postgres row consumption also converted mutablelist to array in some places

--- postgresql-async/src/main/java/com/github/jasync/sql/db/postgresql/parsers/DataRowParser.kt
+++ postgresql-async/src/main/java/com/github/jasync/sql/db/postgresql/parsers/DataRowParser.kt
@@ -1,6 +1,5 @@
 package com.github.jasync.sql.db.postgresql.parsers
 
-import com.github.jasync.sql.db.util.length
 import com.github.jasync.sql.db.postgresql.messages.backend.DataRowMessage
 import com.github.jasync.sql.db.postgresql.messages.backend.ServerMessage
 import io.netty.buffer.ByteBuf
@@ -9,16 +8,15 @@ import io.netty.buffer.ByteBuf
 object DataRowParser : MessageParser {
 
   override fun parseMessage(buffer: ByteBuf): ServerMessage {
-    val row = mutableListOf<ByteBuf?>()
-    0.until(row.length).forEach { column ->
+    val row = Array(buffer.readShort().toInt()) {
       val length = buffer.readInt()
-      row[column] = if (length == -1) {
+      if (length == -1) {
         null
       } else {
         buffer.readBytes(length)
       }
     }
-    return DataRowMessage(row.toTypedArray())
+    return DataRowMessage(row)
   }
 
 }
