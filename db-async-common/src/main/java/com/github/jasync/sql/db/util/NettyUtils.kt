035784f51cf9b7f774b5a7a4a9d29420341343ac
oshai <ohadshai@gmail.com>
2019-01-03 21:26:58 +0200

    fix #67 - epoll support

--- db-async-common/src/main/java/com/github/jasync/sql/db/util/NettyUtils.kt
+++ db-async-common/src/main/java/com/github/jasync/sql/db/util/NettyUtils.kt
@@ -1,16 +1,31 @@
 package com.github.jasync.sql.db.util
 
+import io.netty.channel.EventLoopGroup
+import io.netty.channel.epoll.Epoll
+import io.netty.channel.epoll.EpollEventLoopGroup
 import io.netty.channel.nio.NioEventLoopGroup
 import io.netty.util.internal.logging.InternalLoggerFactory
 import io.netty.util.internal.logging.Slf4JLoggerFactory
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger {}
 
 object NettyUtils {
 
     init {
         InternalLoggerFactory.setDefaultFactory(Slf4JLoggerFactory.INSTANCE)
+        if (Epoll.isAvailable()) {
+            logger.info { "selected transport - native (epoll)" }
+        } else {
+            logger.info { "selected transport - nio" }
+        }
     }
 
-    val DefaultEventLoopGroup: NioEventLoopGroup by lazy {
-        NioEventLoopGroup(0, DaemonThreadsFactory("db-sql-netty"))
+    val DefaultEventLoopGroup: EventLoopGroup by lazy {
+        if (Epoll.isAvailable()) {
+            EpollEventLoopGroup(0, DaemonThreadsFactory("db-sql-netty"))
+        } else {
+            NioEventLoopGroup(0, DaemonThreadsFactory("db-sql-netty"))
+        }
     }
 }
