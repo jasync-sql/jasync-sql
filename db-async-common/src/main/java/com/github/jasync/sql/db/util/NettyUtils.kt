8a8c359b0af7885c158efcd4f2068dba3f632f4a
oshai <ohadshai@gmail.com>
2019-01-05 00:54:34 +0200

    make dependency on native transport as optional

--- db-async-common/src/main/java/com/github/jasync/sql/db/util/NettyUtils.kt
+++ db-async-common/src/main/java/com/github/jasync/sql/db/util/NettyUtils.kt
@@ -21,23 +21,31 @@ object NettyUtils {
     init {
         InternalLoggerFactory.setDefaultFactory(Slf4JLoggerFactory.INSTANCE)
         when {
-            Epoll.isAvailable() -> logger.info { "jasync available transport - native (epoll)" }
-            KQueue.isAvailable() -> logger.info { "jasync available transport - native (kqueue)" }
+            tryOrFalse { Epoll.isAvailable() } -> logger.info { "jasync available transport - native (epoll)" }
+            tryOrFalse { KQueue.isAvailable() } -> logger.info { "jasync available transport - native (kqueue)" }
             else -> logger.info { "jasync selected transport - nio" }
         }
     }
 
     val DefaultEventLoopGroup: EventLoopGroup by lazy {
         when {
-            Epoll.isAvailable() -> EpollEventLoopGroup(0, DaemonThreadsFactory("db-sql-netty"))
-            KQueue.isAvailable() -> KQueueEventLoopGroup(0, DaemonThreadsFactory("db-sql-netty"))
+            tryOrFalse { Epoll.isAvailable() } -> EpollEventLoopGroup(0, DaemonThreadsFactory("db-sql-netty"))
+            tryOrFalse { KQueue.isAvailable() } -> KQueueEventLoopGroup(0, DaemonThreadsFactory("db-sql-netty"))
             else -> NioEventLoopGroup(0, DaemonThreadsFactory("db-sql-netty"))
         }
     }
 
     fun getSocketChannelClass(eventLoopGroup: EventLoopGroup): Class<out SocketChannel> = when {
-        eventLoopGroup is EpollEventLoopGroup -> EpollSocketChannel::class.java
-        eventLoopGroup is KQueueEventLoopGroup -> KQueueSocketChannel::class.java
+        tryOrFalse { eventLoopGroup is EpollEventLoopGroup } -> EpollSocketChannel::class.java
+        tryOrFalse { eventLoopGroup is KQueueEventLoopGroup } -> KQueueSocketChannel::class.java
         else -> NioSocketChannel::class.java
+    }
+
+    private fun tryOrFalse(fn: () -> Boolean): Boolean {
+        return try {
+            fn.invoke()
+        } catch (t: Throwable) {
+            false
+        }
     }
 }
