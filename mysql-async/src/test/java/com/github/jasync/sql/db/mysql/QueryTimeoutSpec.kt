49d7eeab186b477cd1417a2f92bf520894662f7c
Ohad Shai <ohadshai@gmail.com>
2019-01-08 19:29:56 +0200

    fix QueryTimeoutSpec (#75)

--- mysql-async/src/test/java/com/github/jasync/sql/db/mysql/QueryTimeoutSpec.kt
+++ mysql-async/src/test/java/com/github/jasync/sql/db/mysql/QueryTimeoutSpec.kt
@@ -1,9 +1,13 @@
 package com.github.jasync.sql.db.mysql
 
-import com.github.jasync.sql.db.Configuration
+import com.github.jasync.sql.db.exceptions.ConnectionTimeoutedException
 import org.assertj.core.api.Assertions.assertThat
+import org.awaitility.kotlin.await
+import org.awaitility.kotlin.matches
+import org.awaitility.kotlin.untilCallTo
 import org.junit.Test
 import java.time.Duration
+import java.util.concurrent.ExecutionException
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.TimeoutException
 
@@ -12,55 +16,40 @@ class QueryTimeoutSpec : ConnectionHelper() {
     @Test
     fun `"Simple query with 1 nanosec timeout"`() {
         withConfigurablePool(shortTimeoutConfiguration()) { pool ->
-            {
-                val connection = pool.take().get(10, TimeUnit.SECONDS)
-                assertThat(connection.isTimeout()).isEqualTo(false)
-                assertThat(connection.isConnected()).isEqualTo(true)
-                val queryResultFuture = connection.sendQuery("select sleep(1)")
-                verifyException(TimeoutException::class.java) {
-                    queryResultFuture.get(10, TimeUnit.SECONDS)
-                }
-                assertThat(connection.isTimeout()).isEqualTo(true)
-                pool.giveBack(connection).get(10, TimeUnit.SECONDS)
-                assertThat(pool.availables().size).isEqualTo(0) // connection removed from pool)
-                // we do not know when the connection will be closed.
+            val connection = pool.take().get(10, TimeUnit.SECONDS)
+            assertThat(connection.isTimeout()).isEqualTo(false)
+            assertThat(connection.isConnected()).isEqualTo(true)
+            val queryResultFuture = connection.sendQuery("select sleep(100)")
+            verifyException(ExecutionException::class.java, TimeoutException::class.java) {
+                queryResultFuture.get(10, TimeUnit.SECONDS)
             }
+            await.untilCallTo { connection.isTimeout()} matches { it == true }
+            verifyException(ExecutionException::class.java, ConnectionTimeoutedException::class.java) {
+                pool.giveBack(connection).get(10, TimeUnit.SECONDS)
+            }
+            await.untilCallTo { pool.idleConnectionsCount } matches { it == 0 } // connection removed from pool
         }
     }
 
     @Test
     fun `"Simple query with 5 sec timeout"`() {
         withConfigurablePool(longTimeoutConfiguration()) { pool ->
-            {
-                val connection = pool.take().get(10, TimeUnit.SECONDS)
-                assertThat(connection.isTimeout()).isEqualTo(false)
-                assertThat(connection.isConnected()).isEqualTo(true)
-                val queryResultFuture = connection.sendQuery("select sleep(1)")
-                assertThat((queryResultFuture.get(10, TimeUnit.SECONDS)).rows.size).isEqualTo(1)
-                assertThat(connection.isTimeout()).isEqualTo(false)
-                assertThat(connection.isConnected()).isEqualTo(true)
-                pool.giveBack(connection).get(10, TimeUnit.SECONDS)
-                assertThat(pool.availables().size).isEqualTo(1) // connection returned to pool)
-            }
+            val connection = pool.take().get(10, TimeUnit.SECONDS)
+            assertThat(connection.isTimeout()).isEqualTo(false)
+            assertThat(connection.isConnected()).isEqualTo(true)
+            val queryResultFuture = connection.sendQuery("select sleep(1)")
+            assertThat((queryResultFuture.get(10, TimeUnit.SECONDS)).rows.size).isEqualTo(1)
+            assertThat(connection.isTimeout()).isEqualTo(false)
+            assertThat(connection.isConnected()).isEqualTo(true)
+            pool.giveBack(connection).get(10, TimeUnit.SECONDS)
+            await.untilCallTo { pool.idleConnectionsCount } matches { it == 1 } // connection returned to pool
         }
     }
 
-    fun shortTimeoutConfiguration() = Configuration(
-        "mysql_async",
-        "localhost",
-        port = 3306,
-        password = ("root"),
-        database = ("mysql_async_tests"),
-        queryTimeout = (Duration.ofNanos(1))
-    )
+    private fun shortTimeoutConfiguration() =
+        ContainerHelper.defaultConfiguration.copy(queryTimeout = (Duration.ofNanos(1)))
 
-    private fun longTimeoutConfiguration() = Configuration(
-        "mysql_async",
-        "localhost",
-        port = 3306,
-        password = ("root"),
-        database = ("mysql_async_tests"),
-        queryTimeout = (Duration.ofSeconds(5))
-    )
+    private fun longTimeoutConfiguration() =
+        ContainerHelper.defaultConfiguration.copy(queryTimeout = (Duration.ofSeconds(5)))
 
 }
