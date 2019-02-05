213c6ded8c19c927678d2b1616a0d9601fb1d641
oshai <ohadshai@gmail.com>
2019-02-05 15:33:11 +0200

    update var name

--- db-async-common/src/main/java/com/github/jasync/sql/db/QueryListener.kt
+++ db-async-common/src/main/java/com/github/jasync/sql/db/QueryListener.kt
@@ -3,9 +3,9 @@ package com.github.jasync.sql.db
 interface QueryListener {
     fun onQuery(query: String)
     fun onQueryComplete(result: QueryResult)
-    fun onQueryError(query: Throwable)
+    fun onQueryError(throwable: Throwable)
 
     fun onPreparedStatement(query: String, values: List<Any?>)
     fun onPreparedStatementComplete(result: QueryResult)
-    fun onPreparedStatementError(query: Throwable)
+    fun onPreparedStatementError(throwable: Throwable)
 }
