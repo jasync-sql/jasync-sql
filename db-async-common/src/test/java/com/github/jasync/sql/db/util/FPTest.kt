b52555cf12104fcdc85ec06fb0a9751e8cb45894
oshai <ohadshai@gmail.com>
2019-02-11 15:11:11 +0200

    add more tests

---
+++ db-async-common/src/test/java/com/github/jasync/sql/db/util/FPTest.kt
@@ -0,0 +1,36 @@
+package com.github.jasync.sql.db.util
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import java.util.concurrent.ExecutionException
+
+
+class FPTest {
+
+    @Test(expected = ExecutionException::class)
+    fun failed() {
+        val future = FP.failed<String>(Exception())
+        future.get()
+    }
+
+    @Test
+    fun successful() {
+        val future = FP.successful("a")
+        assertThat(future.get()).isEqualTo("a")
+        assertThat(future.isSuccess).isTrue()
+        assertThat(future.isFailure).isFalse()
+
+    }
+
+    @Test
+    fun `test flatMap`() {
+        assertThat(FP.successful("a").flatMap { FP.successful("$it$it") }.get()).isEqualTo("aa")
+    }
+
+    @Test
+    fun `test onComplete`() {
+        var completed = false
+        assertThat(FP.successful("a").onComplete { completed = true }.get()).isEqualTo("a")
+        assertThat(completed).isTrue()
+    }
+}
