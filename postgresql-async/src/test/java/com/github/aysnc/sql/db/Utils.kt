d70496de54f7b99c640cb711da248adea353bc6b
Andy Chen <andy.yx.chen@outlook.com>
2019-03-05 14:22:59 -0800

    removed comment line

--- postgresql-async/src/test/java/com/github/aysnc/sql/db/Utils.kt
+++ postgresql-async/src/test/java/com/github/aysnc/sql/db/Utils.kt
@@ -11,7 +11,6 @@ fun verifyException(
         body()
         throw Exception("Expected exception was not thrown: ${exType.simpleName}->${causeType?.simpleName}")
     } catch (e: Exception) {
-        e.printStackTrace()
         Assertions.assertThat(e::class.java).isEqualTo(exType)
         causeType?.let { Assertions.assertThat(e.cause!!::class.java).isEqualTo(it) }
         return e.cause ?: e
