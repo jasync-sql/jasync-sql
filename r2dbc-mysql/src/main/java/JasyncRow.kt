734c2c2b2b0f89a2bc7014d0d71704de9d6903df
oshai <ohadshai@gmail.com>
2019-03-19 14:16:41 +0200

    add explicit type conversions

--- r2dbc-mysql/src/main/java/JasyncRow.kt
+++ r2dbc-mysql/src/main/java/JasyncRow.kt
@@ -2,35 +2,63 @@ package com.github.jasync.r2dbc.mysql
 
 import com.github.jasync.sql.db.RowData
 import io.r2dbc.spi.Row
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.LocalTime
 
 
 class JasyncRow(private val rowData: RowData) : Row {
 
 
+    @Suppress("UNCHECKED_CAST", "IMPLICIT_CAST_TO_ANY")
     override fun <T> get(identifier: Any, requestedType: Class<T>): T? {
-
-
-        if (identifier is String) {
-            return rowData.getAs(identifier)
-        }
-
-        if (identifier is Int) {
-            return rowData.getAs(identifier)
-        }
-
-        throw IllegalArgumentException("Identifier must be a String or an Integer")
+        val value = get(identifier)
+        return when {
+            requestedType == String::class.java -> value?.toString()
+            value is Number -> {
+                when (requestedType) {
+                    Long::class.java -> value.toLong()
+                    Int::class.java -> value.toInt()
+                    Float::class.java -> value.toFloat()
+                    Double::class.java -> value.toDouble()
+                    Char::class.java -> value.toChar()
+                    Short::class.java -> value.toShort()
+                    Byte::class.java -> value.toByte()
+                    else -> throw IllegalStateException("unmatched requested type ${requestedType.simpleName}")
+                }
+            }
+            value is org.joda.time.LocalDateTime -> {
+                when (requestedType) {
+                    LocalDate::class.java -> value.toLocalDate().jodaToJavaLocalDate()
+                    LocalDateTime::class.java -> value.jodaToJavaLocalDateTime()
+                    LocalTime::class.java -> value.toLocalTime().jodaToJavaLocalTime()
+                    else -> throw IllegalStateException("unmatched requested type ${requestedType.simpleName}")
+                }
+            }
+            value is org.joda.time.LocalDate -> {
+                when (requestedType) {
+                    LocalDate::class.java -> value.jodaToJavaLocalDate()
+                    LocalDateTime::class.java -> value.toLocalDateTime(org.joda.time.LocalTime.MIDNIGHT).jodaToJavaLocalDateTime()
+                    LocalTime::class.java -> org.joda.time.LocalTime.MIDNIGHT.jodaToJavaLocalTime()
+                    else -> throw IllegalStateException("unmatched requested type ${requestedType.simpleName}")
+                }
+            }
+            value is org.joda.time.LocalTime -> {
+                when (requestedType) {
+                    LocalTime::class.java -> value.jodaToJavaLocalTime()
+                    else -> throw IllegalStateException("unmatched requested type ${requestedType.simpleName}")
+                }
+            }
+            else -> requestedType.cast(value)
+        }  as T?
     }
 
     override fun get(identifier: Any): Any? {
-
-        if (identifier is String) {
-            return rowData.get(identifier)
+        return when (identifier) {
+            is String -> rowData[identifier]
+            is Int -> rowData[identifier]
+            else -> throw IllegalArgumentException("Identifier must be a String or an Integer")
         }
-
-        if (identifier is Int) {
-            return rowData.get(identifier)
-        }
-
-        throw IllegalArgumentException("Identifier must be a String or an Integer")
     }
 }
+
