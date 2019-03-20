4325665e0d07f8bb42437511498e380c205129c9
Mark Paluch <mpaluch@pivotal.io>
2019-03-20 12:49:26 +0100

    Add implementation for transactional methods
    JasyncClientConnection now supports BEGIN/COMMIT/ROLLBACK and SAVEPOINT methods.

--- r2dbc-mysql/src/main/java/JasyncClientConnection.kt
+++ r2dbc-mysql/src/main/java/JasyncClientConnection.kt
@@ -6,37 +6,42 @@ import io.r2dbc.spi.Connection
 import io.r2dbc.spi.IsolationLevel
 import io.r2dbc.spi.Statement
 import org.reactivestreams.Publisher
+import reactor.core.publisher.Mono
 import reactor.core.publisher.toMono
 import java.util.function.Supplier
 import com.github.jasync.sql.db.Connection as JasyncConnection
 
 class JasyncClientConnection(private val jasyncConnection: JasyncConnection) : Connection, Supplier<JasyncConnection> {
-    override fun rollbackTransactionToSavepoint(name: String): Publisher<Void> {
-        TODO("not implemented")
-    }
 
-    override fun rollbackTransaction(): Publisher<Void> {
-        TODO("not implemented")
-    }
-
-    override fun setTransactionIsolationLevel(isolationLevel: IsolationLevel): Publisher<Void> {
-        TODO("not implemented")
+    override fun beginTransaction(): Publisher<Void> {
+        return executeVoid("START TRANSACTION")
     }
 
     override fun commitTransaction(): Publisher<Void> {
-        TODO("not implemented")
+        return executeVoid("COMMIT")
     }
 
     override fun createSavepoint(name: String): Publisher<Void> {
-        TODO("not implemented")
+        assertValidSavepointName(name)
+        return executeVoid("SAVEPOINT `$name`")
     }
 
     override fun releaseSavepoint(name: String): Publisher<Void> {
-        TODO("not implemented")
+        assertValidSavepointName(name)
+        return executeVoid("RELEASE SAVEPOINT `$name`")
     }
 
-    override fun beginTransaction(): Publisher<Void> {
-        TODO("not implemented")
+    override fun rollbackTransactionToSavepoint(name: String): Publisher<Void> {
+        assertValidSavepointName(name)
+        return executeVoid("ROLLBACK TO SAVEPOINT `$name`")
+    }
+
+    override fun rollbackTransaction(): Publisher<Void> {
+        return executeVoid("ROLLBACK")
+    }
+
+    override fun setTransactionIsolationLevel(isolationLevel: IsolationLevel): Publisher<Void> {
+        return executeVoid("SET TRANSACTION ISOLATION LEVEL ${isolationLevel.asSql()}")
     }
 
     override fun createStatement(sql: String): Statement {
@@ -44,7 +49,7 @@ class JasyncClientConnection(private val jasyncConnection: JasyncConnection) : C
     }
 
     override fun close(): Publisher<Void> {
-        return jasyncConnection.disconnect().toMono().map { null }
+        return Mono.defer { jasyncConnection.disconnect().toMono().then() };
     }
 
     override fun createBatch(): Batch {
@@ -53,5 +58,17 @@ class JasyncClientConnection(private val jasyncConnection: JasyncConnection) : C
 
     override fun get(): JasyncConnection {
         return jasyncConnection
+    }
+
+    private fun executeVoid(sql: String) =
+            Mono.defer({ jasyncConnection.sendQuery(sql).toMono().then() })
+
+    private fun assertValidSavepointName(name: String) {
+        if (name.isEmpty()) {
+            throw IllegalArgumentException("Savepoint name is empty")
+        }
+        if (name.indexOf('`') != -1) {
+            throw IllegalArgumentException("Savepoint name must not contain backticks")
+        }
     }
 }
