fc6ec12895b6e5c36d2d69c92ec758b8f386ef30
oshai <ohadshai@gmail.com>
2019-03-26 22:43:27 +0200

    Add MysqlConnectionFactoryProvider

---
+++ r2dbc-mysql/src/main/java/MysqlConnectionFactoryProvider.kt
@@ -0,0 +1,51 @@
+package com.github.jasync.r2dbc.mysql
+
+import com.github.jasync.sql.db.Configuration
+import com.github.jasync.sql.db.mysql.pool.MySQLConnectionFactory
+import io.r2dbc.spi.ConnectionFactoryOptions
+import io.r2dbc.spi.ConnectionFactoryOptions.CONNECT_TIMEOUT
+import io.r2dbc.spi.ConnectionFactoryOptions.DRIVER
+import io.r2dbc.spi.ConnectionFactoryOptions.HOST
+import io.r2dbc.spi.ConnectionFactoryOptions.PASSWORD
+import io.r2dbc.spi.ConnectionFactoryOptions.PORT
+import io.r2dbc.spi.ConnectionFactoryOptions.USER
+import io.r2dbc.spi.ConnectionFactoryProvider
+import io.r2dbc.spi.Option
+
+
+class MysqlConnectionFactoryProvider : ConnectionFactoryProvider {
+
+    /**
+     * Application name.
+     */
+    val APPLICATION_NAME: Option<String> = Option.valueOf("applicationName")
+
+    /**
+     * Driver option value.
+     */
+    val MYSQL_DRIVER = "mysql"
+
+    @Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
+    override fun create(connectionFactoryOptions: ConnectionFactoryOptions): JasyncConnectionFactory {
+        val configuration = Configuration(
+            host = connectionFactoryOptions.getValue(HOST),
+            port = connectionFactoryOptions.getValue(PORT),
+            username = connectionFactoryOptions.getValue(USER),
+            password = connectionFactoryOptions.getValue(PASSWORD)?.toString(),
+            applicationName = connectionFactoryOptions.getValue(APPLICATION_NAME),
+            connectionTimeout = connectionFactoryOptions.getValue(CONNECT_TIMEOUT)?.toMillis()?.toInt() ?: 5000
+            )
+        return JasyncConnectionFactory(MySQLConnectionFactory(configuration))
+    }
+
+    override fun supports(connectionFactoryOptions: ConnectionFactoryOptions): Boolean {
+        val driver = connectionFactoryOptions.getValue(DRIVER)
+        return when {
+            driver == null || driver != MYSQL_DRIVER -> false
+            !connectionFactoryOptions.hasOption(HOST) -> false
+            !connectionFactoryOptions.hasOption(PORT) -> false
+            !connectionFactoryOptions.hasOption(USER) -> false
+            else -> true
+        }
+    }
+}
