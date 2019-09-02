034e1cb838251ea4fcf1924fd3b20ed8182fc6d0
Versus Void <chaoskeepr@mail.ru>
2019-09-02 09:12:03 +0300

    Update postgres-javalin sample

--- samples/postgres-javalin/src/main/java/oshai/Main.java
+++ samples/postgres-javalin/src/main/java/oshai/Main.java
@@ -3,11 +3,9 @@ package oshai;
 import com.github.jasync.sql.db.Configuration;
 import com.github.jasync.sql.db.Connection;
 import com.github.jasync.sql.db.QueryResult;
-import com.github.jasync.sql.db.pool.ConnectionPool;
-import com.github.jasync.sql.db.pool.PoolConfiguration;
-import com.github.jasync.sql.db.postgresql.pool.PostgreSQLConnectionFactory;
+import com.github.jasync.sql.db.ConnectionPoolConfigurationBuilder;
+import com.github.jasync.sql.db.postgresql.PostgreSQLConnectionBuilder;
 import io.javalin.Javalin;
-import io.javalin.JavalinEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,31 +16,29 @@ public class Main {
   private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
   public static void main(String[] args) {
-    PoolConfiguration poolConfiguration = new PoolConfiguration(
-      100,                            // maxObjects
-      TimeUnit.MINUTES.toMillis(15),  // maxIdle
-      10_000,                         // maxQueueSize
-      TimeUnit.SECONDS.toMillis(30)   // validationInterval
-    );
-    Connection connection = new ConnectionPool<>(
-      // for PostgreSQL use PostgreSQLConnectionFactory instead of MySQLConnectionFactory
-      new PostgreSQLConnectionFactory(new Configuration(
-        "postgres",
-        "localhost",
-        5432,
-        "mysecretpassword",
-        "postgres"
-      )), poolConfiguration);
+    ConnectionPoolConfigurationBuilder config = new ConnectionPoolConfigurationBuilder();
+    config.setUsername("postgres");
+    config.setPassword("mysecretpassword");
+    config.setDatabase("postgres");
+    config.setMaxActiveConnections(100);
+    config.setMaxIdleTime(TimeUnit.MINUTES.toMillis(15));
+    config.setMaxPendingQueries(10000);
+    config.setConnectionValidationInterval(TimeUnit.SECONDS.toMillis(30));
+
+    // for PostgreSQL use PostgreSQLConnectionBuilder instead of MySQLConnectionBuilder
+    Connection connection = PostgreSQLConnectionBuilder.createConnectionPool(config);
     Javalin app = Javalin.create()
-      .event(JavalinEvent.SERVER_STARTING, () -> {
-        logger.info("--- SERVER STARTING!");
-        connection.connect().get();
-        logger.info("--- connection STARTED!");
-      })
-      .event(JavalinEvent.SERVER_STOPPING, () -> {
-        logger.info("--- SERVER STOPPING!");
-        connection.disconnect().get();
-        logger.info("--- connection STOPPED!");
+      .events(event -> {
+          event.serverStarting(() -> {
+              logger.info("--- SERVER STARTING!");
+              connection.connect().get();
+              logger.info("--- connection STARTED!");
+          });
+          event.serverStopping(() -> {
+              logger.info("--- SERVER STOPPING!");
+              connection.disconnect().get();
+              logger.info("--- connection STOPPED!");
+          });
       })
       .start(7000);
 
