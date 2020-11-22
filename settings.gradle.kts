2b7683dfc0c5d32b76dd4b174ef89537712fce57
Ohad Shai <ohadshai@gmail.com>
2020-11-22 21:58:12 +0200

    automate snapshot and release deployments (#210)
    * upgrade gradle 6.4.1 -> 6.6.1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,11 +9,13 @@ include(
 pluginManagement {
     val KOTLIN_VERSION: String by settings
     val BINTRAY_VERSION: String by settings
+    val ARTIFACTORY_VERSION: String by settings
     val KTLINT_VERSION: String by settings
 
     plugins {
         kotlin("jvm") version KOTLIN_VERSION
         id("com.jfrog.bintray") version BINTRAY_VERSION
+        id("com.jfrog.artifactory") version ARTIFACTORY_VERSION
         id("org.jlleitschuh.gradle.ktlint") version KTLINT_VERSION
     }
 }
