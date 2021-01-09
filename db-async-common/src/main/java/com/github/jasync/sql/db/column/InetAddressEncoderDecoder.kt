e0f9a6c0f5e4f883231d1570bc62ea975821227e
Dustin <dustin.johnson@seeq.com>
2021-01-09 12:49:31 -0800

    Correct minor compiler warnings/errors

--- db-async-common/src/main/java/com/github/jasync/sql/db/column/InetAddressEncoderDecoder.kt
+++ db-async-common/src/main/java/com/github/jasync/sql/db/column/InetAddressEncoderDecoder.kt
@@ -1,20 +1,8 @@
 package com.github.jasync.sql.db.column
 
 import java.net.InetAddress
-import sun.net.util.IPAddressUtil.textToNumericFormatV4
-import sun.net.util.IPAddressUtil.textToNumericFormatV6
 
 object InetAddressEncoderDecoder : ColumnEncoderDecoder {
-
-    override fun decode(value: String): Any {
-        return if (value.contains(':')) {
-            InetAddress.getByAddress(textToNumericFormatV6(value))
-        } else {
-            InetAddress.getByAddress(textToNumericFormatV4(value))
-        }
-    }
-
-    override fun encode(value: Any): String {
-        return (value as InetAddress).hostAddress
-    }
+    override fun decode(value: String): Any = InetAddress.getByName(value)
+    override fun encode(value: Any): String = (value as InetAddress).hostAddress
 }
