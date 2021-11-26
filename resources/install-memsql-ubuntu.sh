20dad1e00f1dbc3d820ce4b176b5d33d9d7b1a44
Ohad Shai <oshai@google.com>
2021-11-26 22:18:11 +0200

    try 22

--- resources/install-memsql-ubuntu.sh
+++ resources/install-memsql-ubuntu.sh
@@ -2,9 +2,9 @@
 wget http://download.memsql.com/memsql-ops-4.0.35/memsql-ops-4.0.35.tar.gz
 tar -xzf memsql-ops-4.0.35.tar.gz
 cd memsql-ops-4.0.35
-sudo su -
-mkdir /var/lib/memsql
-echo "minimum_core_count = 2" > /var/lib/memsql/memsql.cnf
-./install.sh
+
+sudo mkdir /var/lib/memsql
+sudo sh -c 'echo "minimum_core_count = 2" > /var/lib/memsql/memsql.cnf'
+sudo ./install.sh
 
 echo memsql is ready!
