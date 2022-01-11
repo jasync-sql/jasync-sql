869ebf03e02ce6b4170317ecf4eb133d44109f7d
Ohad Shai <oshai@google.com>
2022-01-11 23:52:32 +0200

    fixes to search path test

--- postgresql-async/src/test/java/com/github/aysnc/sql/db/postgresql/QuerySpec.kt
+++ postgresql-async/src/test/java/com/github/aysnc/sql/db/postgresql/QuerySpec.kt
@@ -1,15 +1,16 @@
-package com.github.jasync.sql.db.postgresql
+package com.github.aysnc.sql.db.postgresql
 
 import com.github.aysnc.sql.db.integration.ContainerHelper
 import com.github.aysnc.sql.db.integration.DatabaseTestHelper
 import com.github.aysnc.sql.db.verifyException
+import com.github.jasync.sql.db.postgresql.exceptions.GenericDatabaseException
 import com.github.jasync.sql.db.util.length
+import java.util.concurrent.ExecutionException
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
-import java.util.concurrent.ExecutionException
 
 class QuerySpec : DatabaseTestHelper() {
-    val messagesCreate = """CREATE TEMP TABLE messages
+    private val messagesCreate = """CREATE TABLE if not exists testing.messages
                          (
                            id bigserial NOT NULL,
                            content character varying(255) NOT NULL,
@@ -17,10 +18,10 @@ class QuerySpec : DatabaseTestHelper() {
                          )"""
 
     @Test
-    fun `connection should be able to select from a table`() {
+    fun `connection should be able to select from a table on search path`() {
+        init()
         withHandler(ContainerHelper.defaultConfiguration.copy(currentSchema = "testing")) { handler ->
-            executeDdl(handler, this.messagesCreate)
-            executeDdl(handler, "INSERT INTO messages (content) VALUES ('Hello')")
+            executeQuery(handler, "INSERT INTO messages (content) VALUES ('Hello')")
         }
 
         withHandler { handler ->
@@ -29,21 +30,28 @@ class QuerySpec : DatabaseTestHelper() {
         }
     }
 
-    @Test
-    fun `connection should be able to namespaced to a schema`() {
-        withHandler(ContainerHelper.defaultConfiguration.copy(currentSchema = "testing")) { handler ->
+    private fun init() {
+        withHandler { handler ->
+            executeDdl(handler, "create schema if not exists testing")
+            executeDdl(handler, "drop table if exists testing.messages")
             executeDdl(handler, this.messagesCreate)
-            executeDdl(handler, "INSERT INTO messages (content) VALUES ('Hello')")
+        }
+    }
+
+    @Test
+    fun `error if table without using search path`() {
+        init()
+        withHandler(ContainerHelper.defaultConfiguration.copy(currentSchema = "testing")) { handler ->
+            executeQuery(handler, "INSERT INTO messages (content) VALUES ('Hello')")
         }
 
         withHandler { handler ->
-            val rows = executePreparedStatement(handler, "SELECT id FROM messages").rows
+            val rows = executePreparedStatement(handler, "SELECT id FROM testing.messages").rows
             assertThat(rows.length).isEqualTo(1)
 
-            verifyException(ExecutionException::class.java) {
+            verifyException(ExecutionException::class.java, GenericDatabaseException::class.java) {
                 executePreparedStatement(handler, "SELECT id FROM messages")
             }
         }
     }
-
 }
