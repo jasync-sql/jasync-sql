b2e430c61daac27dce53f88090e9398f15411ae3
czp3009 <czp3009@gmail.com>
2022-02-14 17:07:37 +0800

    newest spring sample

--- samples/spring-kotlin/src/main/kotlin/Application.kt
+++ samples/spring-kotlin/src/main/kotlin/Application.kt
@@ -2,15 +2,10 @@ package me.pgs
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
-@ComponentScan
 class Application
 
 fun main(args: Array<String>) {
     runApplication<Application>(*args)
 }
-
-
-
