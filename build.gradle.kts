56074cf33854b17355b4a1ae527d44768dddc39a
oshai <ohadshai@gmail.com>
2022-10-05 17:36:22 +0300

    bump version to 2.1.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ apply(plugin = "io.github.gradle-nexus.publish-plugin")
 allprojects {
 
     group = "com.github.jasync-sql"
-    version = "2.1.5"
+    version = "2.1.6"
 
     apply(plugin = "kotlin")
     apply(plugin = "maven-publish")
