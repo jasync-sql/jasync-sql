98e613d1f474377cfcf7b4818c6a896e0bdac054
oshai <ohadshai@gmail.com>
2022-10-13 13:16:20 +0300

    more changes

--- postgis-jasync/src/test/java/PostgisSpec.kt
+++ postgis-jasync/src/test/java/PostgisSpec.kt
@@ -15,6 +15,8 @@ class PostgisSpec : DatabaseTestHelper() {
         PostgreSQLColumnDecoderRegistry.Instance.registerType(Geom.GeometryColumnType, JtsColumnDecoder())
     }
 
+    private val lineString = WKTReader(GeometryFactory(PrecisionModel(), 4326)).read("LINESTRING(1 2, 3 4)")
+
     @Test
     fun `simple query`() {
         withHandler { handler ->
@@ -22,7 +24,20 @@ class PostgisSpec : DatabaseTestHelper() {
             assertThat(res1.rows[0][0].toString()).contains("POSTGIS=")
 //            val res2 = executeQuery(handler, "SELECT ST_GeomFromText('POINT(1 2)',4326)")
             val res2 = executeQuery(handler, "SELECT ST_GeomFromText('LINESTRING(1 2, 3 4)',4326)")
-            assertThat(res2.rows[0][0]).isEqualTo(WKTReader(GeometryFactory(PrecisionModel(), 4326)).read("LINESTRING(1 2, 3 4)"))
+            assertThat(res2.rows[0][0]).isEqualTo(lineString)
+        }
+    }
+
+    @Test
+    fun `insert and query`() {
+        withHandler { handler ->
+            executeQuery(handler, "DROP TABLE if exists postgis_geom_test")
+            executeQuery(handler, "CREATE TABLE postgis_geom_test (geom geometry NOT NULL)")
+            val insertRes = executePreparedStatement(handler, "insert into postgis_geom_test (geom) values (?)", listOf(lineString))
+            assertThat(insertRes.rowsAffected).isEqualTo(1L)
+            val res = executeQuery(handler, "SELECT geom from postgis_geom_test")
+            val geom = res.rows[0][0]
+            assertThat(geom).isEqualTo(WKTReader(GeometryFactory(PrecisionModel(), 4326)).read("LINESTRING(1 2, 3 4)"))
         }
     }
 }
