d0be1fe5c14e8663fc663889e657a56d11d4a7ef
oshai <ohadshai@gmail.com>
2023-01-13 12:59:34 +0200

    an initial impl for r2dbc filter/flatmap
    on operation
    create a new instance from result `JasyncSegmentResult`
    JasyncSegmentResult has a flux of segments which are
    rows + number of rows segment.
    Then filter and flatmap are operating on that flux.
    
    There is no error handling here as the original result
    don't have such concept.
    JasyncStatement.execute() either yields result or exception (exception is not a result).
    
    see #351

--- r2dbc-mysql/src/main/java/JasyncResult.kt
+++ r2dbc-mysql/src/main/java/JasyncResult.kt
@@ -2,6 +2,8 @@ package com.github.jasync.r2dbc.mysql
 
 import com.github.jasync.sql.db.ResultSet
 import io.r2dbc.spi.Result
+import io.r2dbc.spi.Result.RowSegment
+import io.r2dbc.spi.Row
 import io.r2dbc.spi.RowMetadata
 import org.reactivestreams.Publisher
 import reactor.core.publisher.Flux
@@ -34,18 +36,60 @@ class JasyncResult(
 
     override fun <T> map(mappingFunction: BiFunction<io.r2dbc.spi.Row, RowMetadata, out T>): Publisher<T> {
         return if (selectLastInsertId) {
-            Mono.fromSupplier { mappingFunction.apply(JasyncInsertSyntheticRow(generatedKeyName, lastInsertId), JasyncInsertSyntheticMetadata(generatedKeyName)) }
+            Mono.fromSupplier {
+                mappingFunction.apply(
+                    JasyncInsertSyntheticRow(generatedKeyName, lastInsertId),
+                    JasyncInsertSyntheticMetadata(generatedKeyName)
+                )
+            }
         } else {
             Flux.fromIterable(resultSet)
-                .map { mappingFunction.apply(JasyncRow(it), metadata) }
+                .map { mappingFunction.apply(JasyncRow(it, metadata), metadata) }
         }
     }
 
     override fun filter(filter: Predicate<Result.Segment>): Result {
-        TODO("Not yet implemented")
+        return JasyncSegmentResult(this).filter(filter)
     }
 
     override fun <T : Any?> flatMap(mappingFunction: Function<Result.Segment, out Publisher<out T>>): Publisher<T> {
-        TODO("Not yet implemented")
+        return JasyncSegmentResult(this).flatMap(mappingFunction)
+    }
+
+    class JasyncSegmentResult private constructor(
+        private val segments: Flux<Result.Segment>,
+        private val result: JasyncResult
+    ) : Result {
+        constructor(result: JasyncResult) : this(
+            Flux.concat(
+                Flux.fromIterable(result.resultSet)
+                    .map { JasyncRow(it, result.metadata) },
+                Flux.just(Result.UpdateCount { result.rowsAffected })
+            ),
+            result
+        )
+
+        override fun getRowsUpdated(): Publisher<Long> {
+            return result.rowsUpdated
+        }
+
+        override fun <T : Any?> map(mappingFunction: BiFunction<Row, RowMetadata, out T>): Publisher<T> {
+            return segments
+                .handle { segment, sink ->
+                    if (segment is RowSegment) {
+                        sink.next(mappingFunction.apply(segment.row(), segment.row().metadata))
+                    }
+                }
+        }
+
+        override fun filter(filter: Predicate<Result.Segment>): Result {
+            return JasyncSegmentResult(segments.filter(filter), result)
+        }
+
+        override fun <T : Any?> flatMap(mappingFunction: Function<Result.Segment, out Publisher<out T>>): Publisher<T> {
+            return segments.concatMap { segment: Result.Segment ->
+                mappingFunction.apply(segment)
+            }
+        }
     }
 }
---
+++ r2dbc-mysql/src/main/java/JasyncSegmentResult.kt
@@ -0,0 +1,1 @@
+package com.github.jasync.r2dbc.mysql
