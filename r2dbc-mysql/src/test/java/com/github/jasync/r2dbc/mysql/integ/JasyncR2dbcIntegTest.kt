69397a187fcf7f8fa4ebda13cb77f442bcb449a3
oshai <ohadshai@gmail.com>
2023-01-16 21:56:16 +0200

    test bing

--- r2dbc-mysql/src/test/java/com/github/jasync/r2dbc/mysql/integ/JasyncR2dbcIntegTest.kt
+++ r2dbc-mysql/src/test/java/com/github/jasync/r2dbc/mysql/integ/JasyncR2dbcIntegTest.kt
@@ -6,12 +6,15 @@ import com.github.jasync.sql.db.mysql.pool.MySQLConnectionFactory
 import com.github.jasync.sql.db.util.FP
 import io.mockk.mockk
 import io.r2dbc.spi.Result
+import mu.KotlinLogging
 import org.assertj.core.api.Assertions
 import org.awaitility.kotlin.await
 import org.junit.Test
 import reactor.core.publisher.Mono
 import java.math.BigDecimal
 import java.util.concurrent.CompletableFuture
+
+private val logger = KotlinLogging.logger {}
 
 class JasyncR2dbcIntegTest : R2dbcConnectionHelper() {
 
@@ -93,6 +96,40 @@ class JasyncR2dbcIntegTest : R2dbcConnectionHelper() {
                 .doOnNext { rows++ }
                 .subscribe()
             await.until { rows == 1 }
+        }
+    }
+    @Test
+    fun `bind test`() {
+        withConnection { c ->
+            var rows = 0
+            executeQuery(c, createTable)
+            executeQuery(c, """INSERT INTO users (name) VALUES ('Boogie Man'),('Dambeldor')""")
+            val mycf = object : MySQLConnectionFactory(mockk()) {
+                override fun create(): CompletableFuture<MySQLConnection> {
+                    return FP.successful(c)
+                }
+            }
+            val cf = JasyncConnectionFactory(mycf)
+            Mono.from(cf.create())
+                .flatMapMany { connection ->
+                    connection
+                        .createStatement("SELECT name FROM users where name in (?, ?)")
+                        .bind(0, "Dambeldor")
+                        .bind(1, "Boogie Man")
+                        .execute()
+                }
+                .flatMap { result ->
+                    result.map { row, rowMetadata ->
+                        logger.info { "got row $row" }
+                        rows++
+                        row.get(0) as String
+                    }
+                }
+                .doOnNext {
+                    logger.info { "next row $it" }
+                }
+                .subscribe()
+            await.until { rows == 2 }
         }
     }
 }
