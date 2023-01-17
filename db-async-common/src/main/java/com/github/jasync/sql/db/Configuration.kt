1db8b05058f02ce39c11d6b7d79e6b2c112eb329
oshai <ohadshai@gmail.com>
2023-01-17 10:18:29 +0200

    fix for #359

--- db-async-common/src/main/java/com/github/jasync/sql/db/Configuration.kt
+++ db-async-common/src/main/java/com/github/jasync/sql/db/Configuration.kt
@@ -63,7 +63,7 @@ data class Configuration @JvmOverloads constructor(
     val executionContext: Executor = ExecutorServiceUtils.CommonPool,
     val currentSchema: String? = null,
     val socketPath: String? = null,
-    private val credentialsProvider: CredentialsProvider? = null
+    val credentialsProvider: CredentialsProvider? = null
 ) {
     init {
         if (socketPath != null && eventLoopGroup is NioEventLoopGroup) {
@@ -75,6 +75,49 @@ data class Configuration @JvmOverloads constructor(
     }
 
     fun resolveCredentials(): CompletionStage<Credentials> = (credentialsProvider ?: StaticCredentialsProvider(username, password)).provide()
+
+    @JvmOverloads
+    fun copyConfiguration(
+        username: String? = null,
+        host: String? = null,
+        port: Int? = null,
+        password: String? = null,
+        database: String? = null,
+        ssl: SSLConfiguration? = null,
+        charset: Charset? = null,
+        maximumMessageSize: Int? = null,
+        allocator: ByteBufAllocator? = null,
+        connectionTimeout: Int? = null,
+        queryTimeout: Duration? = null,
+        applicationName: String? = null,
+        interceptors: List<Supplier<QueryInterceptor>>? = null,
+        eventLoopGroup: EventLoopGroup? = null,
+        executionContext: Executor? = null,
+        currentSchema: String? = null,
+        socketPath: String? = null,
+        credentialsProvider: CredentialsProvider? = null,
+    ): Configuration {
+        return Configuration(
+            username = username ?: this.username,
+            host = host ?: this.host,
+            port = port ?: this.port,
+            password = password ?: this.password,
+            database = database ?: this.database,
+            ssl = ssl ?: this.ssl,
+            charset = charset ?: this.charset,
+            maximumMessageSize = maximumMessageSize ?: this.maximumMessageSize,
+            allocator = allocator ?: this.allocator,
+            connectionTimeout = connectionTimeout ?: this.connectionTimeout,
+            queryTimeout = queryTimeout ?: this.queryTimeout,
+            applicationName = applicationName ?: this.applicationName,
+            interceptors = interceptors ?: this.interceptors,
+            eventLoopGroup = eventLoopGroup ?: this.eventLoopGroup,
+            executionContext = executionContext ?: this.executionContext,
+            currentSchema = currentSchema ?: this.currentSchema,
+            socketPath = socketPath ?: this.socketPath,
+            credentialsProvider = credentialsProvider ?: this.credentialsProvider,
+        )
+    }
 }
 
 fun Configuration.toDebugString(): String {
