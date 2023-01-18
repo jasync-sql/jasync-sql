f8fbd791b9263601318e94d070550a1c112cfbd7
oshai <ohadshai@gmail.com>
2023-01-19 00:02:59 +0200

    add ability to get SQLSTATE from Postgres GenericDatabaseException
    Fixes #277

--- postgresql-async/src/main/java/com/github/jasync/sql/db/postgresql/exceptions/GenericDatabaseException.kt
+++ postgresql-async/src/main/java/com/github/jasync/sql/db/postgresql/exceptions/GenericDatabaseException.kt
@@ -2,5 +2,15 @@ package com.github.jasync.sql.db.postgresql.exceptions
 
 import com.github.jasync.sql.db.exceptions.DatabaseException
 import com.github.jasync.sql.db.postgresql.messages.backend.ErrorMessage
+import com.github.jasync.sql.db.postgresql.messages.backend.InformationMessage
 
-class GenericDatabaseException(val errorMessage: ErrorMessage) : DatabaseException(errorMessage.toString())
+class GenericDatabaseException(val errorMessage: ErrorMessage) : DatabaseException(errorMessage.toString()) {
+
+    /**
+     * get SQLSTATEinfo
+     */
+    fun getSQLState(): String? {
+        val sqlStateField = errorMessage.fields.keys.firstOrNull { "SQLSTATE" == InformationMessage.fieldName(it) }
+        return if (sqlStateField === null) null else errorMessage.fields[sqlStateField]
+    }
+}
