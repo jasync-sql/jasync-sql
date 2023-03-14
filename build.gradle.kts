d1403ea67727f22417517a5624c89f8d68bd680d
oshai <ohadshai@gmail.com>
2023-03-14 20:42:25 +0200

    bump version to 2.0.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ apply(plugin = "io.github.gradle-nexus.publish-plugin")
 allprojects {
 
     group = "com.github.jasync-sql"
-    version = "2.0.9"
+    version = "2.0.10"
 
     apply(plugin = "kotlin")
     apply(plugin = "maven-publish")
