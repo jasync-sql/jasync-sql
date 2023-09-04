06bedd16d37c0e606c6da6ebfed45a87eb7084aa
oshai <ohadshai@gmail.com>
2023-09-05 00:03:02 +0300

    bump version to 2.2.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ apply(plugin = "io.github.gradle-nexus.publish-plugin")
 allprojects {
 
     group = "com.github.jasync-sql"
-    version = "2.2.4"
+    version = "2.2.5"
 
     apply(plugin = "kotlin")
     apply(plugin = "maven-publish")
